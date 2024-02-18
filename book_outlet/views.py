1b02177a9922ca008d06c1e34e1b56b841aa3054
rafsun-riad <mruhaquer@gmail.com>
2024-02-18 19:37:27 +0600

    made dynamic

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+
 from .models import Book
 
 
@@ -9,4 +11,22 @@ def index(request):
         request,
         "book_outlet/index.html",
         {"books": books},
+    )
+
+
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(pk=id)
+    except:
+        raise Http404()
+
+    return render(
+        request,
+        "book_outlet/book_detail.html",
+        {
+            "title": book.title,
+            "author": book.author,
+            "rating": book.rating,
+            "is_bestseller": book.is_bestselling,
+        },
     )
