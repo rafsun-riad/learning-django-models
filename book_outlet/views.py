0f3795e38eed9d92e4f0515388d1c6a2c6d6999d
rafsun-riad <mruhaquer@gmail.com>
2024-02-20 20:33:15 +0600

    learned django admin

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.db.models import Avg
 
 from .models import Book
 
@@ -7,10 +8,17 @@ from .models import Book
 # Create your views here.
 def index(request):
     books = Book.objects.all()
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
     return render(
         request,
         "book_outlet/index.html",
-        {"books": books},
+        {
+            "books": books,
+            "total_number_of_books": num_books,
+            "average_rating": avg_rating,
+        },
     )
 
 
