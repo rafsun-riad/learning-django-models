42095775a2e996468705fea03d1ff455748cf0ee
rafsun-riad <mruhaquer@gmail.com>
2024-02-22 21:02:36 +0600

    updated models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,13 +5,17 @@ from django.shortcuts import reverse
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(Author, on_delete= models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
 
