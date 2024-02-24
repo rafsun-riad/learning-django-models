71950361e17962c82fed34e070c700e1bd812e5c
rafsun-riad <mruhaquer@gmail.com>
2024-02-24 20:26:22 +0600

    learned 1 to 1 and meta class

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -6,9 +6,23 @@ from django.shortcuts import reverse
 # Create your models here.
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
